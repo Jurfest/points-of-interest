97e5387b369e899694a0bc6826e30f63d2a26530
Jurfest <jurfest.apps@gmail.com>
2021-10-29 19:48:30 -0300

    main: generating visitor component in visitor lib, cleaning tour template and style and adding router module

--- apps/tour/src/app/app.module.ts
+++ apps/tour/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: 'tour',
+        loadChildren: () =>
+          import('@packt/visitor').then((m) => m.VisitorModule),
+      },
+      { path: '', pathMatch: 'full', redirectTo: 'tour' },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
